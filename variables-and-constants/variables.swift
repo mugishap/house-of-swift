236ad1586c0c5ece78f56c846184d8d78355feca
mugishap <precieuxmugisha@gmail.com>
2022-12-25 20:58:56 +0200

    [feat]: Adding input variables

---
+++ variables-and-constants/variables.swift
@@ -0,0 +1,6 @@
+var someVariableName:String = "This is a variable"
+print("This is a sample output from me");
+
+var inputString:String = readLine();
+
+print("This is what you entered: \(inputString)");
