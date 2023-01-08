3fc72509d243df9de92f8d0d481918393edb37fb
Mugisha Precieux <94197472+mugishap@users.noreply.github.com>
2023-01-08 18:42:35 +0200

    Delete variables-and-constants directory

--- variables-and-constants/variables.swift
+++
@@ -1,6 +0,0 @@
-var someVariableName:String = "This is a variable"
-print("This is a sample output from me");
-
-var inputString:String = readLine();
-
-print("This is what you entered: \(inputString)");
