890dd4324d37da8cba2d2405c227545f978197f8
mugishap <precieuxmugisha@gmail.com>
2023-01-16 07:37:20 +0200

    Nil coalescing

--- optionals/optionals.swift
+++ optionals/optionals.swift
@@ -7,3 +7,14 @@ if let oldestAge = ages.last{
 } else{
     print("The array is empty!!!")
 }
+
+//nil coalescing
+let oldestAge: Int = ages.last ??  999 
+    //Using nil coalescing can cause pyramid of doom!!!
+
+//guard statement
+func getOldestAge(){
+    guard let oldestAge: Int  = ages.last else {   
+    }
+    //Guard avoids pyramid of doom 
+}
